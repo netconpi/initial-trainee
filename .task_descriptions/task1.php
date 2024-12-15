f700555e45d354ac811617a0dd746409843f385c
ntcad <i@ntcad.ru>
2024-12-15 22:45:34 +0300

    feat(feat): new feat

--- .task_descriptions/task1.php
+++ .task_descriptions/task1.php
@@ -1,5 +1,16 @@
 <main role="main" class="container">
     <h1 class="mt-5">Задание: </h1>
-    <p class="lead">Описание задания</p>
-    <p>Дополнительный параграф описания</p>
-</main>
+    <p class="lead">Есть массив чисел сгенерированный данным кодом: </p>
+    <pre class="alert alert-light"><code>
+        $arrNumber = [];
+        for ($i = 0; $i &lt; 15; $i++) {
+            $arrNumber[] = rand();
+        }
+    </code></pre>
+    <p>У вас уже доступен массив (сгенерированный) $arrNumber</p>
+    <p>Вам нужно найти и вывести ВСЕ ЧЕТНЫЕ числа данного массива</p>
+
+    <iframe class="container" style="height: 570px" src="https://code.trainee.dterra.ru/?folder=/var/www/www/b7f85b1ce473/" frameborder="0"></iframe>
+
+    <h1 class="mt-5">Результат работы: </h1>
+    <pre class="alert alert-light"><code>
